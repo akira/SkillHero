be58dcd04a73f87d7f63db36f44c46946c88ce03
Alex Kira <alex@getbeezy.com>
2011-10-11 22:24:06 -0700

    initial version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StartupWeekend::Application.config.secret_token = 'a7464f98fa1147e894f04ed53c80bc9197a5a06cca311cf335d84b02c9cf7dcb5fcf161ae1e8e4237fabd872863a75f74896d786ee7b8fb152ed4d076144d1cb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+StartupWeekend::Application.config.session_store :cookie_store, key: '_StartupWeekend_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# StartupWeekend::Application.config.session_store :active_record_store
